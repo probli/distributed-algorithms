133980fbfe54ba85b40c77c9357b09b48d69182f
ping1z <zhongyp.design@gmail.com>
2018-03-23 07:32:17 -0500

    init project (#8)
    Edges are not ordered

---
+++ synchGHS/src/MsgAction.java
@@ -0,0 +1,5 @@
+public class MsgAction {
+    public static final String TEST = "TEST";
+    public static final String CONNECT = "CONNECT";
+    public static final String DISCONNECT = "DISCONNECT";
+}
---
+++ synchGHS/src/MsgFactory.java
@@ -0,0 +1,36 @@
+public class MsgFactory {
+    private static int localNodeId = 0;
+    public static void setLocalNodeId(int id) {
+        localNodeId = id;
+    }
+
+    public static Msg testMsg(Node node) {
+        Msg msg = new Msg();
+        msg.setAction(MsgAction.TEST);
+        msg.setSrcId(node.getId());
+        msg.setFromId(node.getId());
+        msg.setRound(node.getRound());
+        return msg;
+    }
+
+    public static Msg connectMsg(int toId) {
+        Msg msg = new Msg();
+        msg.setAction(MsgAction.CONNECT);
+        msg.setSrcId(localNodeId);
+        msg.setFromId(localNodeId);
+        msg.setToId(toId);
+        msg.setRound(-1);
+        return msg;
+    }
+
+    public static Msg disconnectMsg(int toId) {
+        Msg msg = new Msg();
+        msg.setAction(MsgAction.DISCONNECT);
+        msg.setSrcId(localNodeId);
+        msg.setFromId(localNodeId);
+        msg.setToId(toId);
+        msg.setRound(-1);
+        return msg;
+    }
+
+}
