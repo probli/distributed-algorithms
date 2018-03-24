28821f040f86cb1e80b4d4ff873d43e29e796c7d
lidaxia2017 <mr.liwei1991@gmail.com>
2018-03-23 19:54:46 -0500

    SynchGHS

--- synchGHS/src/MsgAction.java
+++ synchGHS/src/MsgAction.java
@@ -1,5 +1,11 @@
 public class MsgAction {
-    public static final String TEST = "TEST";
     public static final String CONNECT = "CONNECT";
     public static final String DISCONNECT = "DISCONNECT";
+    public static final String SEARCH = "SEARCH";
+    public static final String TEST = "TEST";
+    public static final String REPLY = "REPLY";
+    public static final String CONVERGE = "CONVERGE";
+    public static final String MERGE = "MERGE";
+    public static final String JOIN = "JOIN";
+    public static final String TERMINATE = "TERMINATE";
 }
